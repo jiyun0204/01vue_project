c0dca4d80818402fc551e6bbb540d7ae69bfcd05
jiyun0204 <bomchoon0204@gmail.com>
2021-06-10 10:54:45 +0900

    data

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <banner/>
+
+  <div class="black-bg" v-if="proView==true">
+  <div class="white-bg">
+ <img v-bind:src="product[proNum].image">
+        <div>{{product[proNum].title}}</div>
+        <div>{{product[proNum].price}}</div>
+        <div>{{product[proNum].content}}</div>
+       <button v-on:click="proView=false">닫기</button>
+  </div>
+  </div>
+ 
+   <ul class="view">
+     <li v-for="(item,i) in product" :key="i">
+       <img v-bind:src="product[i].image">
+        <div>{{product[i].title}} <span v-on:click="proView=true;proNum=i">[상세보기]</span></div>
+        <div>{{product[i].price}}</div>
+     </li>
+   </ul>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import vdata from './data.js'
+import banner from './components/banner.vue'
+
+
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+    proNum:0,
+     proView:false, 
+    product:vdata,
   }
+},
+components:{
+  banner
+}
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+html,body{height: 100%;}
+   *{margin: 0;padding: 0;}
+  li{list-style: none;}
+  img{width:100%;}
+  .view li {margin-bottom: 20px;}
+  .black-bg {position: fixed; width: 100%; background:rgba(0,0,0,0.5);
+              top: 0; height: 100%; display: flex; justify-content: center; align-items: center;}
+  .white-bg {width:80%; background:rgba(95, 248, 172, 0.63);
+              border-radius:10px; padding: 20px;}
 </style>
---
+++ src/components/banner.vue
@@ -0,0 +1,28 @@
+<template>
+   <div class="banner">
+       {{text}}
+   </div>
+</template>
+
+<script>
+export default {
+    name:'banner',
+    data(){
+     return{
+         text:'안녕하세요'
+     }  
+    }
+}
+</script>
+
+<style>
+    .banner{
+        width: 100%;
+        height:100px;
+        display: flex; justify-content:center;
+        background:#492;
+        align-items:center;
+
+
+    } 
+</style>
