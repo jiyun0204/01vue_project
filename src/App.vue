6780b6311204f7763360b570e7d5637a84f13700
jiyun0204 <bomchoon0204@gmail.com>
2021-06-11 11:14:21 +0900

    modal

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <banner/>
 
-  <div class="black-bg" v-if="proView==true">
+  <!-- <div class="black-bg" v-if="proView==true">
   <div class="white-bg">
  <img v-bind:src="product[proNum].image">
         <div>{{product[proNum].title}}</div>
@@ -9,12 +9,19 @@
         <div>{{product[proNum].content}}</div>
        <button v-on:click="proView=false">닫기</button>
   </div>
-  </div>
+  </div> -->
+  <modal 
+  v-bind:product="product" 
+  v-bind:proView="proView"
+  v-bind:proNum="proNum"
+  @modalClose="proView=false"/>
  
    <ul class="view">
-     <li v-for="(item,i) in product" :key="i">
+     <li v-for="(item,i) in product"
+      v-bind:key="i">
        <img v-bind:src="product[i].image">
-        <div>{{product[i].title}} <span v-on:click="proView=true;proNum=i">[상세보기]</span></div>
+        <div>{{product[i].title}} <span v-on:click="proView=true;proNum=i">
+          [상세보기]</span></div>
         <div>{{product[i].price}}</div>
      </li>
    </ul>
@@ -25,6 +32,7 @@
 <script>
 import vdata from './data.js'
 import banner from './components/banner.vue'
+import modal from './components/modal.vue'
 
 
 
@@ -39,7 +47,8 @@ export default {
   }
 },
 components:{
-  banner
+  banner:banner,
+  modal:modal,
 }
 
 }
